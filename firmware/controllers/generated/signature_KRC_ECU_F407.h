1d3e7241499ce4fe6c14ff565c63d9e3b6d50f53
GitHub gen-configs Action <action@github.com>
2025-04-01 10:39:18 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_KRC_ECU_F407.h
+++ firmware/controllers/generated/signature_KRC_ECU_F407.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 571401783
-#define TS_SIGNATURE "rusEFI master.2025.04.01.KRC_ECU_F407.571401783"
+#define SIGNATURE_HASH 2975262638
+#define TS_SIGNATURE "rusEFI master.2025.04.01.KRC_ECU_F407.2975262638"
