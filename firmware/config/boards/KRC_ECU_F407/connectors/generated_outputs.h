46729746b589f15bd8b4199a2e9c2ae800176a8b
GitHub gen-configs Action <action@github.com>
2025-04-01 13:50:42 +0000

    Auto-generated configs and docs

--- firmware/config/boards/KRC_ECU_F407/connectors/generated_outputs.h
+++ firmware/config/boards/KRC_ECU_F407/connectors/generated_outputs.h
@@ -24,8 +24,8 @@ Gpio GENERATED_OUTPUTS = {
 	Gpio::D7, // TACHO
 	Gpio::B9, // IDLE1
 	Gpio::B8, // IDLE2
-	Gpio::A8, // BOOST
 	Gpio::B7, // FAN
+	Gpio::A8, // BOOST
 	Gpio::D5, // CEL
 	Gpio::D6, // SPARE
 	Gpio::E10, // A/C_RLY
@@ -50,8 +50,8 @@ Gpio GENERATED_OUTPUTS = {
 // "TACHO",
 // "IDLE1",
 // "IDLE2",
-// "BOOST",
 // "FAN",
+// "BOOST",
 // "CEL",
 // "SPARE",
 // "A/C_RLY",
--- firmware/controllers/generated/signature_KRC_ECU_F407.h
+++ firmware/controllers/generated/signature_KRC_ECU_F407.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2941937215
-#define TS_SIGNATURE "rusEFI master.2025.04.01.KRC_ECU_F407.2941937215"
+#define SIGNATURE_HASH 4177061663
+#define TS_SIGNATURE "rusEFI master.2025.04.01.KRC_ECU_F407.4177061663"
