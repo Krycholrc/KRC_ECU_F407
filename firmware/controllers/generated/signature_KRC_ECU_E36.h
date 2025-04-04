9a7f047f0ed6af24acf1b399ade74eff36d97891
GitHub gen-configs Action <action@github.com>
2025-04-04 09:50:37 +0000

    Auto-generated configs and docs

---
+++ firmware/controllers/generated/signature_KRC_ECU_E36.h
@@ -0,0 +1,6 @@
+//
+// was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
+//
+
+#define SIGNATURE_HASH 4177061663
+#define TS_SIGNATURE "rusEFI master.2025.04.04.KRC_ECU_E36.4177061663"
