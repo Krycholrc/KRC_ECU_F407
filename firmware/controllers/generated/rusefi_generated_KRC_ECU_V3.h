54c39aee10328ea6432f7aacedcc83cdf501c059
GitHub gen-configs Action <action@github.com>
2025-04-24 10:49:01 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_KRC_ECU_V3.h
+++ firmware/controllers/generated/rusefi_generated_KRC_ECU_V3.h
@@ -1390,7 +1390,7 @@
 #define show_tcu_gauges false
 #define show_test_presets true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 4184429822
+#define SIGNATURE_HASH 1911561078
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1720,6 +1720,7 @@
 #define ts_show_auxserial_pins true
 #define ts_show_bank2_cam1 true
 #define ts_show_bank2_cam2 true
+#define ts_show_baro false
 #define ts_show_brake_pedal_indicator true
 #define ts_show_brake_pedal_pin true
 #define ts_show_cam2 true
@@ -1790,7 +1791,7 @@
 #define ts_show_fuel_trim_cylinder_8 true
 #define ts_show_fuel_trim_cylinder_9 true
 #define ts_show_fuel_trims true
-#define ts_show_full_pinout true
+#define ts_show_full_pinout false
 #define ts_show_gdi false
 #define ts_show_gdi_low_level true
 #define ts_show_globalTriggerAngleOffset true
@@ -1839,7 +1840,7 @@
 #define ts_show_sent true
 #define ts_show_software_knock true
 #define ts_show_speedo_settings true
-#define ts_show_spi true
+#define ts_show_spi false
 #define ts_show_spi1_enable true
 #define ts_show_spi1_pins true
 #define ts_show_spi2_enable true
@@ -1890,7 +1891,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.04.24.KRC_ECU_V3.4184429822"
+#define TS_SIGNATURE "rusEFI master.2025.04.24.KRC_ECU_V3.1911561078"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- firmware/controllers/generated/signature_KRC_ECU_V3.h
+++ firmware/controllers/generated/signature_KRC_ECU_V3.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 4184429822
-#define TS_SIGNATURE "rusEFI master.2025.04.24.KRC_ECU_V3.4184429822"
+#define SIGNATURE_HASH 1911561078
+#define TS_SIGNATURE "rusEFI master.2025.04.24.KRC_ECU_V3.1911561078"
