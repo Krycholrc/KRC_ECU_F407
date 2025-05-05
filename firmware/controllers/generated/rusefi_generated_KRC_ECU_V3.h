e376bea5e6ed2071f638364d12e685e0fb9f1428
GitHub gen-configs Action <action@github.com>
2025-05-05 20:37:18 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_KRC_ECU_V3.h
+++ firmware/controllers/generated/rusefi_generated_KRC_ECU_V3.h
@@ -1390,7 +1390,7 @@
 #define show_tcu_gauges false
 #define show_test_presets true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 51234106
+#define SIGNATURE_HASH 3746002986
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1725,7 +1725,7 @@
 #define ts_show_cam2 true
 #define ts_show_can2 true
 #define ts_show_can_bitrate true
-#define ts_show_can_pins false
+#define ts_show_can_pins true
 #define ts_show_can_wbo true
 #define ts_show_charge_estimation true
 #define ts_show_check_engine false
@@ -1890,7 +1890,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.05.05.KRC_ECU_V3.51234106"
+#define TS_SIGNATURE "rusEFI master.2025.05.05.KRC_ECU_V3.3746002986"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
